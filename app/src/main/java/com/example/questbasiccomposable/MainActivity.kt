51d72012dd42a12bfd1f57381e9361aed5170dc3
safirasn21 <safirasn021@gmai.com>
2024-10-19 22:47:54 +0700

    menambahkan komponen gambar download

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.questbasiccomposable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,6 +69,11 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             )
         )
         Spacer(modifier = Modifier.height(8.dp))
+
+        Image(painter = painterResource(id = R.drawable.download),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
 
 
     }
